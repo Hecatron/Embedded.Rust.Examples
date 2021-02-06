216cb3fb161bb2203f84995683269f4c01bb4398
grbd <hecatrons.workshop@gmail.com>
2021-02-06 00:33:04 +0000

    code updates

---
+++ src/ESP32/blinky/src/wdt.rs
@@ -0,0 +1,52 @@
+use esp32_hal::target;
+
+const WDT_WKEY_VALUE: u32 = 0x50D83AA1;
+
+/*
+pub fn disable_wdts(dp: &mut target::Peripherals) {
+
+    let mut rtccntl = dp.RTCCNTL;
+    let mut timg0 = dp.TIMG0;
+    let mut timg1 = dp.TIMG1;
+
+    // (https://github.com/espressif/openocd-esp32/blob/97ba3a6bb9eaa898d91df923bbedddfeaaaf28c9/src/target/esp32.c#L431)
+    // openocd disables the wdt's on halt
+    // we will do it manually on startup
+    disable_timg_wdts(&mut timg0, &mut timg1);
+    disable_rtc_wdt(&mut rtccntl);
+}
+*/
+
+pub fn disable_rtc_wdt(rtccntl: &mut target::RTCCNTL) {
+    /* Disables the RTCWDT */
+    rtccntl
+        .wdtwprotect
+        .write(|w| unsafe { w.bits(WDT_WKEY_VALUE) });
+    rtccntl.wdtconfig0.modify(|_, w| unsafe {
+        w.wdt_stg0()
+            .bits(0x0)
+            .wdt_stg1()
+            .bits(0x0)
+            .wdt_stg2()
+            .bits(0x0)
+            .wdt_stg3()
+            .bits(0x0)
+            .wdt_flashboot_mod_en()
+            .clear_bit()
+            .wdt_en()
+            .clear_bit()
+    });
+    rtccntl.wdtwprotect.write(|w| unsafe { w.bits(0x0) });
+}
+
+pub fn disable_timg_wdts(timg0: &mut target::TIMG0, timg1: &mut target::TIMG1) {
+    timg0
+        .wdtwprotect
+        .write(|w| unsafe { w.bits(WDT_WKEY_VALUE) });
+    timg1
+        .wdtwprotect
+        .write(|w| unsafe { w.bits(WDT_WKEY_VALUE) });
+
+    timg0.wdtconfig0.write(|w| unsafe { w.bits(0x0) });
+    timg1.wdtconfig0.write(|w| unsafe { w.bits(0x0) });
+}

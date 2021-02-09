222520b5c9dfefa091609b0bda2898e7613a2eed
grbd <hecatrons.workshop@gmail.com>
2021-02-09 17:30:46 +0000

    updates

--- src/ESP32/idf-wrapper/src/lib.rs
+++ src/ESP32/idf-wrapper/src/lib.rs
@@ -3,7 +3,15 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+#[cfg(all(target_arch = "xtensa", target_vendor = "esp32", target_os = "none", target_env = "elf"))]
 fn test1() {
+    // Print text to the console
+    //println!("Hello World!");
+    let mut testint = 0;
+}
+
+#[cfg(all(target_arch = "xtensa", target_vendor = "esp32", target_os = "none", target_env = "elf"))]
+fn test2() {
     // Print text to the console
     //println!("Hello World!");
     let mut testint = 0;

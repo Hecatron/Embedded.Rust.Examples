a839765a0a184cad38467fe7a5e09c379e092d38
grbd <hecatrons.workshop@gmail.com>
2024-03-02 00:54:32 +0000

    updates

--- examples/STM32/blinky1/src/main.rs
+++ examples/STM32/blinky1/src/main.rs
@@ -1,21 +1,17 @@
-//! Blinks an LED
-
 #![deny(unsafe_code)]
 #![deny(warnings)]
 #![no_main]
 #![no_std]
 
 extern crate panic_halt;
-
 use stm32f7xx_hal as hal;
-
 use crate::hal::{pac, prelude::*};
 use cortex_m_rt::entry;
 
+// Blinks an LED
 #[entry]
 fn main() -> ! {
     let p = pac::Peripherals::take().unwrap();
-
     let gpioi = p.GPIOI.split();
     let mut led = gpioi.pi1.into_push_pull_output();
 

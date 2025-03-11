8fdfc9150ea4d15897cfe39d1bd00fd2f51b9070
grbd <hecatrons.workshop@gmail.com>
2025-03-11 22:36:06 +0000

    updates

--- src/ESP32/ESP32-Wrover-Kit-Std/src/main.rs
+++ src/ESP32/ESP32-Wrover-Kit-Std/src/main.rs
@@ -1,3 +1,8 @@
+use std::thread::sleep;
+use std::time::Duration;
+use esp_idf_hal::gpio::*;
+use esp_idf_hal::peripherals::Peripherals;
+
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
@@ -8,8 +13,25 @@ fn main() {
 
     log::info!("Hello, world!");
 
-    //loop {
-    //    sleep(Duration::from_millis(1000));
+    let peripherals = Peripherals::take().unwrap();
+    let mut led_red = PinDriver::output(peripherals.pins.gpio0).unwrap();
+    let mut led_green = PinDriver::output(peripherals.pins.gpio2).unwrap();
+    let mut led_blue = PinDriver::output(peripherals.pins.gpio4).unwrap();
 
-    //}
+    loop {
+        let _ = led_red.set_high();
+        sleep(Duration::from_millis(1000));
+        let _ =led_red.set_low();
+        sleep(Duration::from_millis(1000));
+
+        let _ = led_green.set_high();
+        sleep(Duration::from_millis(1000));
+        let _ =led_green.set_low();
+        sleep(Duration::from_millis(1000));
+
+        let _ = led_blue.set_high();
+        sleep(Duration::from_millis(1000));
+        let _ =led_blue.set_low();
+        sleep(Duration::from_millis(1000));
+    }
 }

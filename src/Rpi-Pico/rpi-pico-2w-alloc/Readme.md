5872c646edca576b65c30088424e86dbb327bd49
grbd <hecatrons.workshop@gmail.com>
2025-03-29 22:04:21 +0000

    updates

---
+++ src/Rpi-Pico/rpi-pico-2w-alloc/Readme.md
@@ -0,0 +1,8 @@
+# Readme
+
+This is an example setup for the Rpi Pico 2W Board
+
+  * https://www.raspberrypi.com/documentation/microcontrollers/pico-series.html#pico2w-technical-specification
+
+This example uses embedded-alloc to allow the use of a heap for items such as a vector
+https://github.com/rust-embedded/embedded-alloc/blob/master/examples/global_alloc.rs
---
+++ src/Rpi-Pico/rpi-pico-2w-alloc/src/main.rs
@@ -0,0 +1,92 @@
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+
+mod rp_meta;
+
+// Linked-List First Fit Heap allocator (feature = "llff")
+use embedded_alloc::LlffHeap as Heap;
+// Two-Level Segregated Fit Heap allocator (feature = "tlsf")
+// use embedded_alloc::TlsfHeap as Heap;
+use alloc::vec::Vec;
+
+#[global_allocator]
+static HEAP: Heap = Heap::empty();
+
+// Ensure we halt the program on panic (if we don't mention this crate it won't be linked)
+use panic_halt as _;
+
+// Depends
+use rp235x_hal as hal;
+use embedded_hal::delay::DelayNs;
+use embedded_hal::digital::OutputPin;
+
+/// External high-speed crystal on the Raspberry Pi Pico 2 board is 12 MHz.
+/// Adjust if your board has a different frequency
+const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+
+/// Entry point to our bare-metal application.
+///
+/// The `#[hal::entry]` macro ensures the Cortex-M start-up code calls this function
+/// as soon as all global variables and the spinlock are initialised.
+///
+/// The function configures the rp235x peripherals, then toggles a GPIO pin in
+/// an infinite loop. If there is an LED connected to that pin, it will blink.
+#[hal::entry]
+fn main() -> ! {
+    // Initialize the allocator BEFORE you use it
+    {
+        use core::mem::MaybeUninit;
+        const HEAP_SIZE: usize = 1024;
+        static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
+        unsafe { HEAP.init(&raw mut HEAP_MEM as usize, HEAP_SIZE) }
+    }
+
+    // Create a new vector on the heap and push a value to it
+    let mut xs = Vec::new();
+    xs.push(1);
+
+    // Grab our singleton objects
+    let mut pac = hal::pac::Peripherals::take().unwrap();
+
+    // Set up the watchdog driver - needed by the clock setup code
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+
+    // Configure the clocks
+    let clocks = hal::clocks::init_clocks_and_plls(
+        XTAL_FREQ_HZ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .unwrap();
+
+    let mut timer = hal::Timer::new_timer0(pac.TIMER0, &mut pac.RESETS, &clocks);
+
+    // The single-cycle I/O block controls our GPIO pins
+    let sio = hal::Sio::new(pac.SIO);
+
+    // Set the pins to their default state
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    // TODO for wireless versions the led is on GPIO0 of the wireless chip
+    //cyw43_arch_gpio_put - https://docs.rs/cyw43/latest/cyw43/
+
+    // Configure GPIO25 as an output - non wireless version
+    let mut led_pin = pins.gpio25.into_push_pull_output();
+    loop {
+        led_pin.set_high().unwrap();
+        timer.delay_ms(500);
+        led_pin.set_low().unwrap();
+        timer.delay_ms(500);
+    }
+}

b87d1d0d2629ccbf319a7d0474d29f7078fff588
grbd <hecatrons.workshop@gmail.com>
2025-07-27 12:07:53 +0100

    updates

--- docs/astro/astro.config.mjs
+++ docs/astro/astro.config.mjs
@@ -2,32 +2,60 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import { ion } from "starlight-ion-theme";
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: 'Embedded Rust Examples',
-    social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/Hecatron/Embedded.Rust.Examples' }],
-    sidebar: [
-      {
-        label: 'Guides',
-        items: [
-          // Each item here is one entry in the navigation menu.
-          { label: 'Example Guide', slug: 'guides/example' },
-        ],
+  integrations: [
+    starlight({
+      title: 'Embedded Rust Examples',
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/Hecatron/Embedded.Rust.Examples' }],
+      logo: {
+        dark: "./src/assets/ion-logo.svg",
+        light: "./src/assets/ion-logo-light.svg",
       },
-      {
-        label: 'Reference',
-        autogenerate: { directory: 'reference' },
-      },
-      {
-        label: 'Boards',
-        autogenerate: { directory: 'boards' },
-      },
-    ],
-    plugins: [ion()],
-  }), sitemap()],
+      lastUpdated: true,
+      pagination: false,
+      sidebar: [
+	{
+          label: "[home] Home",
+          link: "/",
+        },
+        {
+          label: '[book] Boards',
+          autogenerate: { directory: 'boards' },
+        },
+      ],
+      customCss: [
+        "@fontsource-variable/space-grotesk/index.css",
+        "@fontsource/space-mono/400.css",
+        "@fontsource/space-mono/700.css",
+        "./src/styles/global.css",
+      ],
+      plugins: [
+        ion({
+          icons: {
+            iconDir: "./src/icons",
+          },
+          footer: {
+            text: "Hecatronic 2025",
+            links: [
+              {
+                text: "Homepage",
+                href: "https://www.hecatron.com",
+              },
+            ],
+            icons: [
+              {
+                name: "github",
+                href: "https://github.com/Hecatron/Embedded.Rust.Examples",
+              },
+            ],
+          },
+        })
+      ],
+    }),
+  sitemap()
+  ],
   output: "static",
 });

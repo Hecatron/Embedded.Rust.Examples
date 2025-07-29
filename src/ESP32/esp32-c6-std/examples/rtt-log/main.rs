2f020114a620f7e957f5a9c7088b6323478a54c0
grbd <hecatrons.workshop@gmail.com>
2025-07-29 02:00:53 +0100

    updates

--- src/ESP32/esp32-c6-std/examples/rtt-log/main.rs
+++ src/ESP32/esp32-c6-std/examples/rtt-log/main.rs
@@ -1,31 +1,19 @@
-//use esp_idf_hal::delay::FreeRtos;
-//use esp_idf_hal::gpio::*;
-//use esp_idf_hal::peripherals::Peripherals;
-
-//use defmt::info;
-//use esp_hal::clock::CpuClock;
-//use esp_hal::main;
-//use esp_hal::time::{Duration, Instant};
-//use panic_rtt_target as _;
+use esp_idf_hal::delay::FreeRtos;
+use log::info;
 
 fn main() -> anyhow::Result<()> {
     esp_idf_svc::sys::link_patches();
-    //let peripherals = Peripherals::take()?;
-    //let mut led = PinDriver::output(peripherals.pins.gpio4)?;
-
-    //rtt_target::rtt_init_defmt!();
-
-    //let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    //let _peripherals = esp_hal::init(config);
-    //esp_alloc::heap_allocator!(size: 64 * 1024);
+    // We use log instead of defmt with std for the esp32
+    // Due to some compiler issues
+    rtt_target::rtt_init_log!();
 
     loop {
-        //info!("Hello world!");
-        //led.set_high()?;
+        info!("Hello world!");
         // we are sleeping here to make sure the watchdog isn't triggered
-        //FreeRtos::delay_ms(1000);
-
-        //led.set_low()?;
-        //FreeRtos::delay_ms(1000);
+        FreeRtos::delay_ms(1000);
     }
 }
+
+// TODO not sure if this is actually running or outputing at this stage
+// Try a ProS3 board or vanilla ESP32 with an LED output
+// Or check with scope

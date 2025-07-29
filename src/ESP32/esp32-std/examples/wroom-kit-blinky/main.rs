ca71c92470280114d29850e11eb97b1f2867864e
grbd <hecatrons.workshop@gmail.com>
2025-07-30 00:50:48 +0100

    updates

--- src/ESP32/esp32-std/examples/wroom-kit-blinky/main.rs
+++ src/ESP32/esp32-std/examples/wroom-kit-blinky/main.rs
@@ -2,22 +2,12 @@ use esp_idf_hal::delay::FreeRtos;
 use esp_idf_hal::gpio::*;
 use esp_idf_hal::peripherals::Peripherals;
 
-//use defmt::info;
-//use esp_hal::clock::CpuClock;
-//use esp_hal::main;
-//use esp_hal::time::{Duration, Instant};
-//use panic_rtt_target as _;
-
 fn main() -> anyhow::Result<()> {
     esp_idf_svc::sys::link_patches();
     let peripherals = Peripherals::take()?;
     let mut led = PinDriver::output(peripherals.pins.gpio4)?;
 
-    //rtt_target::rtt_init_defmt!();
-    //let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    //let _peripherals = esp_hal::init(config);
     loop {
-        //info!("Hello world!");
         led.set_high()?;
         // we are sleeping here to make sure the watchdog isn't triggered
         FreeRtos::delay_ms(1000);
